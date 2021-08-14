261d13ef23b6ed90b0a91a47dd047c67c6fe40ea
MariamMowafy <61725973+MariamMowafy@users.noreply.github.com>
2021-08-15 01:04:54 +0200

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,13 +10,12 @@ const Footer = () => {
       <LinkList>
         <LinkColumn>
           <LinkTitle>Call</LinkTitle>
-          <LinkItem href="tel:314-343-3432">314-343-3432</LinkItem>
+          <LinkItem href="tel:01112809/8">011128099\8</LinkItem>
         </LinkColumn>
         <LinkColumn>
           <LinkTitle>Email</LinkTitle>
-          <LinkItem href="mailto:contact@jsmastery.com">
-            contact@jsmastery.com
-          </LinkItem>
+          <LinkItem href="mailto:mariammowafy1999@gmail.com">
+mariammowafy1999@gmail.com          </LinkItem>
         </LinkColumn>
       </LinkList>
       <SocialIconsContainer>
